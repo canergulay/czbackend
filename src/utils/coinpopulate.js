fb783589a544ce5f3fee7f752e5d882adb4e8b8e
canergulay <79410189+canergulay@users.noreply.github.com>
2021-08-26 11:39:41 +0300

    Update coinpopulate.js

--- src/utils/coinpopulate.js
+++ src/utils/coinpopulate.js
@@ -10,7 +10,7 @@ async function requester(){
     const options = {
         url: 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/map?sort=cmc_rank&limit=500',
         headers: {
-          'X-CMC_PRO_API_KEY': '83ae5966-d0d2-40ae-85ad-0a680b4b99b8'
+          'HERE, THERE WAS MY API KEY'
         }
     }
 
