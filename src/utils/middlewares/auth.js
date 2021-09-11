e1e71a4bb49c17b70959e8b46d393ac4257f80a2
canergulay <79410189+canergulay@users.noreply.github.com>
2021-09-11 12:38:37 +0300

    Update auth.js

--- src/utils/middlewares/auth.js
+++ src/utils/middlewares/auth.js
@@ -8,9 +8,9 @@ const auth = async (req,res,next) => {
         console.log(token)
         
         const decoded = jwt.verify(token,'cointalkzhdes')
-        const user = await User.findOne({ _id:decoded._id , 'tokens.token': token})
+       
 
-        if(!user){
+        if(!decoded){
             throw new Error()
         }
 
